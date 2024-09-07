fa67a5864b1c304baf449beb5a9a3a1532758278
FIXIT-3491 <97809407+FIXIT-3491@users.noreply.github.com>
2024-09-07 10:58:27 -0700

    add while opmode is active

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/LockingMechanumDriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/LockingMechanumDriveTest.java
@@ -43,51 +43,48 @@ public class LockingMechanumDriveTest extends LinearOpMode {
         LMBLServo = hardwareMap.get(Servo.class, "LMBLS");
 
         waitForStart();
+        while (opModeIsActive()) {
+            //Checking for a or b
+            if (gamepad1.a) {
+                LMActive = true;
+            } else if (gamepad1.b) {
+                LMActive = false;
+            }
 
-        //Checking for a or b
-        if (gamepad1.a){
-            LMActive = true;
-        }
-
-        else if (gamepad1.b){
-            LMActive = false;
-        }
-
-        if(LMActive){
-            lockMecanum();
-        }
-
-        else if (!LMActive){
-            unlockMecanum();
-        }
+            if (LMActive) {
+                lockMecanum();
+            } else if (!LMActive) {
+                unlockMecanum();
+            }
 
 
-        //drive logic
-        double max;
-        double lateral;
+            //drive logic
+            double max;
+            double lateral;
 
-        double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
 
-        if (LMActive)
-            lateral = 0;
-        else
-            lateral = gamepad1.left_stick_x;
+            if (LMActive)
+                lateral = 0;
+            else
+                lateral = gamepad1.left_stick_x;
 
-        double yaw = gamepad1.right_stick_x;
+            double yaw = gamepad1.right_stick_x;
 
-        double FLPower = axial + lateral + yaw;
-        double FRPower = axial - lateral - yaw;
-        double BLPower = axial - lateral + yaw;
-        double BRPower = axial + lateral - yaw;
+            double FLPower = axial + lateral + yaw;
+            double FRPower = axial - lateral - yaw;
+            double BLPower = axial - lateral + yaw;
+            double BRPower = axial + lateral - yaw;
 
-        max = Math.max(Math.abs(FLPower), Math.abs(FRPower));
-        max = Math.max(max, Math.abs(BLPower));
-        max = Math.max(max, Math.abs(BRPower));
-        if (max > 1.0) {
-            FLPower /= max;
-            FRPower /= max;
-            BLPower /= max;
-            BRPower /= max;
+            max = Math.max(Math.abs(FLPower), Math.abs(FRPower));
+            max = Math.max(max, Math.abs(BLPower));
+            max = Math.max(max, Math.abs(BRPower));
+            if (max > 1.0) {
+                FLPower /= max;
+                FRPower /= max;
+                BLPower /= max;
+                BRPower /= max;
+            }
         }
     }
     public void lockMecanum(){
