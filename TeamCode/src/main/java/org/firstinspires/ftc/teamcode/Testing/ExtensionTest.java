950ccaf1526c926862d3be8e2f621ab9ba0d229c
Kai Williams <13iak13@gmail.com>
2024-10-13 13:07:13 -0700

    Made Extension Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ExtensionTest.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.Robot.RobotContainer;
+
+@Autonomous
+
+public class ExtensionTest extends LinearOpMode {
+
+    private DcMotor extensionMotor;
+    private DcMotor liftMotor1;
+    private DcMotor liftMotor2;
+
+    @Override
+    public void runOpMode() {
+
+        extensionMotor = hardwareMap.get(DcMotor.class, "extension");
+        liftMotor1 = hardwareMap.get(DcMotor.class, "Lift1");
+        liftMotor2 = hardwareMap.get(DcMotor.class, "Lift2");
+
+        waitForStart();
+
+        liftMotor1.setPower(0.3);
+        liftMotor2.setPower(-0.3);
+
+    }
+}
