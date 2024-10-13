f2a950f6362eff3a1e5c38865fc73b9130152e5b
Leo Zhang <leopengzhang@gmail.com>
2024-10-13 13:06:08 -0700

    make wrist test for 0 position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ColourSensorValueTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/ColourSensorValueTest.java
@@ -25,3 +25,4 @@ public class ColourSensorValueTest extends LinearOpMode {
             telemetry.update();
         }
     }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/WristTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Testing;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@Autonomous
+public class WristTest extends LinearOpMode{
+    private CRServo intakeMotor;
+    private Servo wristServoOne;
+    private Servo wristServoTwo;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        intakeMotor = hardwareMap.get(CRServo.class, "Spinnny");
+        wristServoOne = hardwareMap.get(Servo.class, "WSO");
+        wristServoTwo = hardwareMap.get(Servo.class, "WST");
+
+        wristServoTwo.setDirection(Servo.Direction.REVERSE);
+
+        waitForStart();
+        wristServoTwo.setPosition(0);
+        wristServoOne.setPosition(0);
+    }
+}
