1061a952c07c4683b5b74f025d536bfdcd9147fb
Leo Zhang <leopengzhang@gmail.com>
2024-10-13 12:23:34 -0700

    make senor logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/PickupMethod.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/PickupMethod.java
@@ -1,4 +1,14 @@
 package org.firstinspires.ftc.teamcode.Commands;
 
 public class PickupMethod {
+
+    //public void SpikeMark {
+//        wristDown();
+//        while (color == !red && blue && yellow){
+//        color = detect color
+//        spinIntake(1);
+//        }
+//        spinIntake(0);
+//        wristUp(); //doesn't work yet
+   // }
 }
