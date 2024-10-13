325a270d7029ec78294b37921eaa77767b9e158e
Kai Williams <13iak13@gmail.com>
2024-10-13 11:32:04 -0700

    Constructed thy 3 newest Methods (Java class thingys) to complete such tasks regarding the systems of Operation of Robot, Enabling of the Grabith of thy Sample and Obtaining Desired Points Using Thy Samples
    Declared lift motors and created extension and lift methods.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/ExtensionSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/ExtensionSubsystem.java
@@ -7,39 +7,46 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 
-public class ExtensionSubsystem {
+public class ExtensionSubsystem{
     private DcMotorEx extensionMotor;
+    private DcMotorEx liftMotor1;
+    private DcMotorEx liftMotor2;
 
 
     public ExtensionSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
 
         extensionMotor = hardwareMap.get(DcMotorEx.class, "extension");
+        liftMotor1 = hardwareMap.get(DcMotorEx.class, "Lift1");
+        liftMotor2 = hardwareMap.get(DcMotorEx.class, "Lift2");
 
     }
 
     public void moveExtension(int pos){
-
+        extensionMotor.setTargetPosition(1);
+        extensionMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        extensionMotor.setPower(0.4);
+    }
+    public void moveLift(int pos){
+        liftMotor1.setTargetPosition(pos);
+        liftMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor1.setPower(0.4);
+        liftMotor2.setTargetPosition(pos);
+        liftMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor2.setPower(0.4);
     }
 
-    // Extension method
-    public void extend() {
-        // Code to extend,
-
-        //
-        //
-
-
-    }
     public void chamberLow() {
-        // Code to retract
+        moveLift(1000);
     }
     public void chamberHigh() {
-        // Code to stop extension
+        moveLift(2000);
     }
     public void bucketLow() {
-        // Code to stop extension
+        moveLift(1250);
+        moveExtension(500);
     }
     public void bucketHigh() {
-        // Code to stop extension
+        moveLift(2500);
+        moveExtension(500);
     }
 }
