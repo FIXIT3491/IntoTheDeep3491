764806c3b5fd6439085a625bdf9169ec03b71306
Kai Williams <91856998+KaizDoodle@users.noreply.github.com>
2024-11-13 18:44:15 -0800

    Update SparkFunOTOSDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SparkFunOTOSDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SparkFunOTOSDrive.java
@@ -39,7 +39,7 @@ public class SparkFunOTOSDrive extends MecanumDrive {
         // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
 
         // RR localizer note: These units are inches and radians.
-        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, Math.toRadians(0));
+        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(-0.048, 7.485, Math.toRadians(0));
 
         // Here we can set the linear and angular scalars, which can compensate for
         // scaling issues with the sensor measurements. Note that as of firmware
@@ -57,8 +57,8 @@ public class SparkFunOTOSDrive extends MecanumDrive {
         // multiple speeds to get an average, then set the linear scalar to the
         // inverse of the error. For example, if you move the robot 100 inches and
         // the sensor reports 103 inches, set the linear scalar to 100/103 = 0.971
-        public double linearScalar = 1.0;
-        public double angularScalar = 1.0;
+        public double linearScalar = 1.032;
+        public double angularScalar = 0.9923;
     }
 
     public static SparkFunOTOSDrive.Params PARAMS = new SparkFunOTOSDrive.Params();
@@ -118,6 +118,11 @@ public class SparkFunOTOSDrive extends MecanumDrive {
         // RR localizer note:
         // The values are passed by reference, so we create variables first,
         // then pass them into the function, then read from them.
+
+
+
+
+
 
         // Reading acceleration worsens loop times by 1ms,
         // but not reading it would need a custom driver and would break compatibility.
