d34d4708139a2a435418093688056d837936e3d2
Leo Zhang <leopengzhang@gmail.com>
2025-01-29 18:47:51 -0800

    create MoveWristCommand

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Custom/MoveWristCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Custom/MoveWristCommand.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode.Commands.Custom;
 
-public class MoveWristCommand {
+import com.arcrobotics.ftclib.command.InstantCommand;
+
+import org.firstinspires.ftc.teamcode.Robot.Subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.Robot.Subsystems.WristSubsystem;
+
+public class MoveWristCommand extends InstantCommand {
+
+    WristSubsystem wrist;
+    double distance = 0;
+
+    public MoveWristCommand(WristSubsystem wrist, double distance) {
+        addRequirements(wrist);
+
+        this.wrist = wrist;
+        this.distance = distance;
+    }
+
+    @Override
+    public void initialize() {wrist.wristMove(distance);}
+
 }
