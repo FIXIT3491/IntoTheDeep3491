9e0e5fda1aeb8d44a3dc2ac910ace7cc1c809575
Leo Zhang <leopengzhang@gmail.com>
2025-01-29 18:59:25 -0800

    wip AutoSpikeIntake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/CommandGroups/AutoSpikeIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/CommandGroups/AutoSpikeIntake.java
@@ -1,10 +1,24 @@
 package org.firstinspires.ftc.teamcode.Commands.CommandGroups;
 
 import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
 
-public class AutoSpikeIntake extends CommandBase {
+import org.firstinspires.ftc.teamcode.Commands.Custom.IntakeSpinCommand;
+import org.firstinspires.ftc.teamcode.Commands.Custom.MoveWristCommand;
+import org.firstinspires.ftc.teamcode.Robot.Subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.Robot.Subsystems.WristSubsystem;
 
-    public AutoSpikeIntake (/*IntakeSubsystem intake, LMECSubsysetm LMEC, SlideSubsystem slides */){
+public class AutoSpikeIntake extends SequentialCommandGroup {
+
+    public AutoSpikeIntake (IntakeSubsystem intake, SlideSubsystem slides, WristSubsystem wrist){
+        addCommands(
+                new MoveWristCommand(wrist, 1),
+                new IntakeSpinCommand(intake, 1),
+                new WaitCommand(1000)
+
+        );
+
 
     }
 }
